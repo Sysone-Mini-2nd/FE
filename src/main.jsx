404d788926d95ca0fa89cbc99714099f02dc1678
KIM_DEAHO <102588838+DHowor1d@users.noreply.github.com>
2025-09-10 14:16:34 +0900

    fix: 린트 수정

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import './index.css'
 import router from './Router.jsx'
-import { AuthProvider } from './hooks/useAuth.jsx'
+import { AuthProvider } from './contexts/AuthContext.js'
 
 createRoot(document.getElementById('root')).render(
 
